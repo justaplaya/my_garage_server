566abf2dc7e1733b22116a7dd88450543985308f
Grigory Trokhinin <accurateandcool@gmail.com>
2023-11-09 13:28:39 +0300

    set

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const aWss = WSServer.getWss();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-// app.options("*", cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  return next();
-});
+app.options("*", cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     return res.status(200).end();
+//   }
+//
+//   return next();
+// });
 app.use(express.json());
 
 app.ws("/", () => {});
