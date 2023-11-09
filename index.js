97fc109683a188a3b1efd87780e936ff33615527
Grigory Trokhinin <accurateandcool@gmail.com>
2023-11-09 13:11:43 +0300

    urd

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const aWss = WSServer.getWss();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.options("*", cors());
+// app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 
 app.ws("/", () => {});
